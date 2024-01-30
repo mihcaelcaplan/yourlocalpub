6daa3496ccd85ccd2f9ff6eec2f151425b5c3545
michael <mcaplan@mike.home>
2024-01-30 13:23:39 +0100

    adding shipping costs to pub data

--- public/publication_list.js
+++ public/publication_list.js
@@ -12,7 +12,8 @@ export default {
         info: <div><p>A Castle by the Sea is a 22-page publication by Brad Gilbert that collates images taken in Ischia, Italy in the summer of 2022. It has been designed to be disassembled allowing you to use images as individual or large-scale prints.</p><br/> <p>“Although the memories might not depict the actual events 100% they still feel like the truth. The image can show something different but it shows the same truth as the memory and it’s nice to have a scene that only appears in your head. Which is what a moment in time really is. Every time I look back on these images it forms that mist of memories and I feel closer to rabbits near the sea.”</p></div>,
         images_location:"/castle_by_the_sea",
         variants: [],
-        price: 15.00
+        price: 15.00,
+        shipping: 0,
     },
     orpheus_st: {
         id:1,
@@ -26,7 +27,8 @@ export default {
         info: "Orpheus Street Paintings is a 57 paged hardback book that collates the work of Nelson Diplexcito. The book introduces you to the artists studio setting and then showcases his body of work ranging from large scale oil on canvas to smaller oil on paper pieces. The book also contains an interview conducted by Jungwon Jay Hur about Diplexcitos practice.",
         images_location:"/orpheus_st_paintings",
         variants:[],
-        price: 30.00
+        price: 30.00,
+        shipping: 0,
     },
     below_bunyan:{
         id:2,
@@ -40,7 +42,8 @@ export default {
         info: "Below Bunyan is a book that represents the diverse artistic spirit of the next generation, captured through compelling details that reveal the interior world of an oft-overlooked phenomenon.",
         images_location:"/below_bunyan",
         variants: [],
-        price: 15.00
+        price: 15.00,
+        shipping: 0,
     },
     babe_rainbow: {
         id: 3,
@@ -54,7 +57,8 @@ export default {
         info: "Mildly High on The Babe Rainbow combines London concert photography with selected lyrics to transport the reader into the audience at the venue. Each copy features a hand typewritten cover making every edition unique. The publication also comes with a black & white darkroom handprint from The Mild High Club at Moth Club.",
         images_location:"/babe_rainbow",
         variants: [],
-        price: 15.00
+        price: 15.00,
+        shipping: 0,
     },
     
 }
